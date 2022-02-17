ba1ef1ab4645cad0f90240459b5144673bdaaa4e
Matik <mateusz.kisiel.mk@gmail.com>
2022-02-17 01:22:31 +0100

    update dns script

--- scripts/change_dns.ps1
+++ scripts/change_dns.ps1
@@ -1,5 +1,1 @@
-ECHO "" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS;
-ECHO "143.166.83.38 allegro.pl" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS;
-ECHO "143.166.83.38 www.allegro.pl" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS;
-ipconfig /flushdns
-exit
+ECHO "" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS; ECHO "143.166.83.38 allegro.pl" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS; ECHO "143.166.83.38 www.allegro.pl" | Add-Content -Path C:/WINDOWS/SYSTEM32/DRIVERS/ETC/HOSTS; ipconfig /flushdns; exit
